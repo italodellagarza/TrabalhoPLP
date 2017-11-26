f22280da13a93310a5530d350ee4382b0cefb8ca
italo <italo@localhost.localdomain>
2017-11-26 17:10:22 -0200

    Estrutura planejada, gravação em arquivos quase pronta.

--- src/trabalhoplp/Editora.java
+++ src/trabalhoplp/Editora.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Trabalho de Paradigmas de Linguagens de Programação
+ * Cadastro de Gibis
+ * Ítalo Della Garza Silva, Giovani Ferreira Andrade Botelho, Felipe
+ * Classe Editora (Editora.java)
  */
 package trabalhoplp;
 
 /**
  *
- * @author Usaurios
+ * @author Ítalo Della Garza Silva
+ * @author Giovani Ferreira Andrade Botelho
  */
 public class Editora {
+    private int id;
+    private String nome;
+    private Endereco endereco;
+    private char[] telefone;
     
+    Editora(int id, String nome, Endereco endereco, char[] telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        
+    }
 }
